af94d196d54c6ff1f82d9df42d520491023d68ab
Ahmed Aly <aaoflaca@gmail.com>
2021-04-17 21:36:34 -0700

    commit 04172021-06

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
 
   app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve("frontend", "build", "index.html"));
     console.log(`MAIN LINK ... ${__dirname}/frontend/build/index.html`);
   });
 } else {
